d96c9a02ee2b4e496bef8c523fa67c11908de2cf
AppolinFotso <appolin.s.fotso@gmail.com>
2024-02-21 10:13:53 +0000

    Added configurations files

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+
+  # cloud {
+  #   organization = "organisation-name"
+  #   workspaces {
+  #     name = "workspace-name"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e5f882be1900e43b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
